7555b9e788fd9345f1d9096bffcbc96644ca4ba2
Tyler Standridge <tstand90@gmail.com>
2020-06-14 12:31:50 -0700

    Updating codetabs shortcode to include the JS file in the codetab.html file

--- static/js/codetabs.js
+++ static/js/codetabs.js
@@ -2,7 +2,6 @@
 /**
  * Scripts which manages Code Toggle tabs.
  */
-var i;
 // store tabs variable
 var allTabs = document.querySelectorAll("[data-toggle-tab]");
 var allPanes = document.querySelectorAll("[data-pane]");
@@ -23,19 +22,19 @@ function toggleTabs(event) {
 	var selectedTabs = document.querySelectorAll("[data-toggle-tab='" + targetKey + "']");
 	var selectedPanes = document.querySelectorAll("[data-pane='" + targetKey + "']");
 
-	for (var i = 0; i < allTabs.length; i++) {
+	for (let i = 0; i < allTabs.length; i++) {
 		allTabs[i].classList.remove("active");
 		allPanes[i].classList.remove("active");
 	}
 
-	for (var i = 0; i < selectedTabs.length; i++) {
+	for (let i = 0; i < selectedTabs.length; i++) {
 		selectedTabs[i].classList.add("active");
 		selectedPanes[i].classList.add("active");
 	}
 
 }
 
-for (i = 0; i < allTabs.length; i++) {
+for (let i = 0; i < allTabs.length; i++) {
 	allTabs[i].addEventListener("click", toggleTabs)
 }
 // Upon page load, if user has a preferred language in its localStorage, tabs are set to it.
